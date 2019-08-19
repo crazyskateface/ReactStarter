90ad4d83363f33ece40fc6ebdbf02b831cd041f7
Travis Boettjer <travis.boettjer@scripps.com>
2019-08-19 11:08:21 -0400

    first app and style

---
+++ src/components/configsApp/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './configsApp.css'
+
+class Configs extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+    render = () => {
+        return (
+            <div className="config">
+                Whats good?
+            </div>
+        )
+    }
+}
+
+export default Configs;
