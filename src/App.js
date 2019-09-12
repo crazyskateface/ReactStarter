283a2fc6d91af46a1762dbcc55863ae7501fe3c0
Travis Boettjer <travis.boettjer@scripps.com>
2019-09-12 13:32:47 -0400

    best change ever

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          jsconf react app
+          Best react app
         </p>
         <a
           className="App-link"
